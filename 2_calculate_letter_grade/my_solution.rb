94de2f2b71bb914e2a44b7a3a1f2ed22ac0a7e3e
Anup <anup-pradhan@hotmail.com>
2014-03-29 18:07:16 -0700

    updated letter_grade -reflections+refactored solution

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Adam Dziuk
 
 # 1. Pseudocode
 
@@ -26,11 +26,35 @@
 
 # 2. Initial Solution
 
-def get_grade(array)
-  sum = 0
-  array.each do |x| sum +=x end
+# def get_grade(array)
+#   sum = 0
+#   array.each do |x| sum +=x end
   
-  average = sum.to_f/array.length
+#   average = sum.to_f/array.length
+  
+#   if (average >= 90)
+#     'A'
+#   elsif (average >= 80)
+#     'B'
+#   elsif (average >=70)
+#     'C'
+#   elsif (average >=60)
+#     'D'
+#   else 
+#     'F'
+#   end
+    
+#     end
+
+    #Refactored Solution
+
+#updated argument from 'array' to 'scores'
+#used array.reduce method 
+    def get_grade(scores)
+  
+ sum =  scores.reduce(:+)
+  
+  average = sum.to_f/scores.length
   
   if (average >= 90)
     'A'
@@ -45,3 +69,19 @@ def get_grade(array)
   end
     
     end
+
+    #Reflections
+#     What parts of your strategy worked? What problems did you face?
+#For this assignment I paired with Adam Dziuk. We both were able to solve this assigment without any issues
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I didnt have any questions as such. I got to learn a new method 'reduce' as part of this challenge
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None. Yes learnt a new method reduce
+# Did you learn any new skills or tricks?
+# Yes learnt a new method reduce
+# How confident are you with each of the learning objectives?
+# Very confident
+# Which parts of the challenge did you enjoy?
+# Pairing with Adam helped me learn some new tricks.
+# Which parts of the challenge did you find tedious?
+# None
